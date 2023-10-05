518892e20e12e95f6cc6a5d9233555557ad258b5
Charley ∆. L <CharleyDL@users.noreply.github.com>
2023-10-05 18:27:36 +0200

    add page result

--- app.py
+++ app.py
@@ -18,105 +18,20 @@ st.set_page_config(page_title='My-Credit : Simulation de crédit',
                    page_icon="assets/favicon-32x32.png",
                    layout='wide')
 
-center_radio()
-
 st.markdown("""
     <style>
-    .stForm [role=formgroup]{
-        background-color: '#000000';
-    }
+    .st-eb [role="formgroup"]{background-color: '#000000';}
     </style>
 """, unsafe_allow_html=True)
-
 
 ## -- Background (if we want it)
 # set_background("assets/background-pawel-czerwinski.jpg")
 
 
-## -- Form
-col1, col2, col3 = st.columns([1, 2, 1])
+if 'init_form' not in st.session_state:
+    st.session_state.init_form = True
 
-with col2:
-    with st.form("My-Credit"):
-        head1, head2, head3 = st.columns([1, 1, 1])
-
-        with head2:
-            st.header("My-Credit")
-
-        st.markdown("----", unsafe_allow_html=True)
-
-        st.markdown(
-            """
-            <div style="text-align:center">
-                <h4>📋 INFORMATIONS PERSONNELLES</h4>
-            </div>
-            """, unsafe_allow_html=True
-        )
-
-        age = st.slider('Votre âge :', 18, 130, 25)
-
-        marital = st.radio("Situation Maritale", ["Célibataire", "Marié.e", "Divorcé.e"],
-                            horizontal=True, label_visibility='hidden')
-        space()
-        education = st.radio("Votre niveau d'étude :", ["Inconnu", "secondaire", "primaire", "tertiaire"], horizontal=True)
-
-        job = st.selectbox("emploi",
-            ("admin.", "inconnu", "chômeur", "gestion", "femme de ménage", 
-             "entrepreneur", "étudiant", "col bleu", "indépendant", "retraité", 
-             "technicien", "services"), 
-             index=None, placeholder="Catégorie d'emploi", label_visibility="hidden")
-
-        space()
-        balance = st.slider('Salaire Moyen Annuel :', min_value=0, max_value=100_000, value=30_000)
-
-        subcol1, subcol2, subcol3 = st.columns([1, 1, 1])
-        with subcol1:
-            default = st.toggle('Crédit en défaut')
-        with subcol2:
-            housing = st.toggle('Prêt Logement')
-        with subcol3:
-            loan = st.toggle('Prêt Personnel')
-
-        st.markdown("----", unsafe_allow_html=True)
-        st.markdown(
-            """
-            <div style="text-align:center; margin-bottom:20px;">
-                <h4>☎️ CONTACT AVEC NOUS</h4>
-            </div>
-            """, unsafe_allow_html=True
-        )
-
-        pdays = st.toggle('Avez vous été contactés par nos services ?')
-
-
-        contact = st.radio("contact_type", ["inconnu", "téléphone", "cellulaire"],
-                            horizontal=True, label_visibility="hidden")
-        space()
-        campaign = st.number_input("Nombre de contact cette année", step=1)
-        previous = st.number_input("Nombre de contact années passées", step=1)
-
-        day_col, month_col = st.columns([1, 1])
-        with day_col:
-            day = st.selectbox("day",
-                (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 
-                 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31),
-                index=None, placeholder="jour du dernier contact", label_visibility="hidden")
-
-        with month_col:
-            month = st.selectbox("month",
-                ("jan", "feb", "mar", "apr", "may", "jun", 
-                "jul", "aug", "sep", "oct", "nov", "dec"),
-                index=None, placeholder="mois du dernier contact", label_visibility="hidden")
-
-        duration = st.number_input("Durée du contact (en seconde)", step=10)
-
-        colF1,colF2,colF3 = st.columns([2, 1, 2])
-        with colF2:
-            space()
-            if st.form_submit_button("Valider"):
-                with st.spinner("Wait a minute"):
-                    result = send_to_api(age, job, marital, education, default, 
-                        balance, housing, loan, contact, day, month, duration, 
-                        campaign, pdays, previous)
-
-
+if st.session_state.init_form:
+        forms()
+else:
+    response_page()
--- functions.py
+++ functions.py
@@ -10,7 +10,9 @@
 import base64
 import requests
 import streamlit as st
-import time
+import os
+import pickle
+
 
 ## --- APP --- ##
 def center_radio():
@@ -58,7 +60,6 @@ def set_background(background_img):
         """,
         unsafe_allow_html=True,
     )
-
 
 
 def send_to_api(age, job, marital, education, default, balance, housing, loan, 
@@ -149,3 +150,129 @@ def send_to_api(age, job, marital, education, default, balance, housing, loan,
 
 def display_score():
     pass
+
+
+def import_pickle_object(pickle_file_path):
+    if os.path.exists(pickle_file_path):
+        with open(pickle_file_path, "rb") as f:
+            pickled_object = pickle.load(f)
+    else:
+        print("The file does not exist.")
+    return pickled_object
+
+
+def get_feature_important():
+    pickel_file_path = "modele_data.pkl"
+    pickled_object = import_pickle_object(pickel_file_path)
+    feature_importance = pickled_object['feature_importance']
+    return feature_importance
+
+
+
+
+## --- FORM PAGE --- ##
+def forms():
+    """Display the 'My-Credit' form"""
+
+    ## - Config Form
+    center_radio()
+
+    ## -- Form
+    col1, col2, col3 = st.columns([1, 2, 1])
+
+    with col2:
+        with st.form("My-Credit"):
+            head1, head2, head3 = st.columns([1, 1, 1])
+
+            with head2:
+                st.header("My-Credit")
+
+            st.markdown("----", unsafe_allow_html=True)
+
+            st.markdown(
+                """
+                <div style="text-align:center">
+                    <h4>📋 INFORMATIONS PERSONNELLES</h4>
+                </div>
+                """, unsafe_allow_html=True
+            )
+
+            age = st.slider('Votre âge :', 18, 130, 25)
+
+            marital = st.radio("Situation Maritale", ["Célibataire", "Marié.e", "Divorcé.e"],
+                horizontal=True, label_visibility='hidden')
+            space()
+            education = st.radio("Votre niveau d'étude :", ["Inconnu", 
+                "secondaire", "primaire", "tertiaire"], horizontal=True)
+
+            job = st.selectbox("emploi",
+                ("admin.", "inconnu", "chômeur", "gestion", "femme de ménage", 
+                "entrepreneur", "étudiant", "col bleu", "indépendant", "retraité", 
+                "technicien", "services"), 
+                index=None, placeholder="Catégorie d'emploi", label_visibility="hidden")
+            space()
+            balance = st.slider('Salaire Moyen Annuel :', min_value=0, max_value=100_000, value=30_000)
+
+            subcol1, subcol2, subcol3 = st.columns([1, 1, 1])
+            with subcol1:
+                default = st.toggle('Crédit en défaut')
+            with subcol2:
+                housing = st.toggle('Prêt Logement')
+            with subcol3:
+                loan = st.toggle('Prêt Personnel')
+
+            st.markdown("----", unsafe_allow_html=True)
+            st.markdown(
+                """
+                <div style="text-align:center; margin-bottom:20px;">
+                    <h4>☎️ CONTACT AVEC NOUS</h4>
+                </div>
+                """, unsafe_allow_html=True
+            )
+
+            pdays = st.toggle('Avez vous été contactés par nos services ?')
+            contact = st.radio("contact_type", ["inconnu", "téléphone", "cellulaire"],
+                                horizontal=True, label_visibility="hidden")
+            space()
+            campaign = st.number_input("Nombre de contact cette année", step=1)
+            previous = st.number_input("Nombre de contact années passées", step=1)
+
+            day_col, month_col = st.columns([1, 1])
+            with day_col:
+                day = st.selectbox("day",
+                    (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 
+                    18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31),
+                    index=None, placeholder="jour du dernier contact", label_visibility="hidden")
+
+            with month_col:
+                month = st.selectbox("month",
+                    ("jan", "feb", "mar", "apr", "may", "jun", 
+                    "jul", "aug", "sep", "oct", "nov", "dec"),
+                    index=None, placeholder="mois du dernier contact", label_visibility="hidden")
+
+            duration = st.number_input("Durée du contact (en seconde)", step=10)
+
+            colF1,colF2,colF3 = st.columns([2, 1, 2])
+            with colF2:
+                space()
+                if st.form_submit_button("Valider"):
+                    with st.spinner("Wait a minute"):
+                        result = send_to_api(age, job, marital, education, default, 
+                            balance, housing, loan, contact, day, month, duration, 
+                            campaign, pdays, previous)
+
+                    # get_feature_important()
+
+                    ## - pass to result
+                    st.session_state.init_form = False 
+                    st.rerun()
+
+
+def response_page():
+    st.write("Ladies & Gentlemen, here the answer !")
+
+
+    st.button("Refaire une simulation", type="primary")
+    if st.button:
+        st.session_state.init_form = True
+        st.rerun
