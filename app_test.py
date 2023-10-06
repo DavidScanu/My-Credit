4fd2bdefbfcb1d3625e539fe5689d67b6c0ccee3
David Scanu <davidscanu14@gmail.com>
2023-10-06 10:21:45 +0200

    Fixing app-test.py

--- app_test.py
+++ app_test.py
@@ -6,7 +6,7 @@ class TestAPI(unittest.TestCase):
 
     client = TestClient(app)
 
-    ex_json_dict = eval("""{'age': 34, 'job': 'entrepreneur', 'marital': 'married', 'education': 'tertiary', 'default': 'yes', 'balance': 35266, 'housing': 'yes', 'loan': 'no', 'contact': 'telephone', 'day': 15, 'month': 'aug', 'duration': 80, 'campaign': 2, 'pdays': 1, 'previous': 5}""")
+    ex_json_dict = {'age': 34, 'job': 'entrepreneur', 'marital': 'married', 'education': 'tertiary', 'default': 'yes', 'balance': 35266, 'housing': 'yes', 'loan': 'no', 'contact': 'telephone', 'day': 15, 'month': 'aug', 'duration': 80, 'campaign': 2, 'pdays': 1, 'previous': 5}
 
     # Test de la fonction
     def test_make_prediction(self):
