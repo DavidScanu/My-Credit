e1b72a72c78135dad5601d70664eda14ad2fe106
David Scanu <davidscanu14@gmail.com>
2023-10-06 11:46:13 +0200

    Update requirements.txt

--- app_test.py
+++ app_test.py
@@ -8,10 +8,40 @@ class TestAPI(unittest.TestCase):
 
     ex_json_dict = {'age': 34, 'job': 'entrepreneur', 'marital': 'married', 'education': 'tertiary', 'default': 'yes', 'balance': 35266, 'housing': 'yes', 'loan': 'no', 'contact': 'telephone', 'day': 15, 'month': 'aug', 'duration': 80, 'campaign': 2, 'pdays': 1, 'previous': 5}
 
+    ex_json_dict_simple = {'age': 34, 'job': 'entrepreneur', 'marital': 'married', 'education': 'tertiary', 'default': 'yes', 'balance': 35266, 'housing': 'yes', 'loan': 'no', 'contact': 'telephone', 'day': 15, 'month': 'aug', 'duration': 80, 'campaign': 2, 'pdays': 1, 'previous': 5}
+
+    ex_json_dict_double = {
+    "age": 34,
+    "job": "entrepreneur",
+    "marital": "married",
+    "education": "tertiary",
+    "default": "yes",
+    "balance": 35266,
+    "housing": "yes",
+    "loan": "no",
+    "contact": "telephone",
+    "day": 15,
+    "month": "aug",
+    "duration": 80,
+    "campaign": 2,
+    "pdays": 1,
+    "previous": 5
+    }
+
+    ex_json_str = """{'age': 34, 'job': 'entrepreneur', 'marital': 'married', 'education': 'tertiary', 'default': 'yes', 'balance': 35266, 'housing': 'yes', 'loan': 'no', 'contact': 'telephone', 'day': 15, 'month': 'aug', 'duration': 80, 'campaign': 2, 'pdays': 1, 'previous': 5}"""
+
     # Test de la fonction
     def test_make_prediction(self):
         self.assertEqual(dict, type(make_prediction(self.ex_json_dict)))
 
-    def test_response_predict(self):
-        response = self.client.post("/predict", json=self.ex_json_dict)
+    def test_response_predict_simple(self):
+        response = self.client.post("/predict", json=self.ex_json_dict_simple)
         self.assertEqual(response.status_code, 200)
+
+    def test_response_predict_double(self):
+        response = self.client.post("/predict", json=self.ex_json_dict_double)
+        self.assertEqual(response.status_code, 200)
+
+    # def test_response_predict_str(self):
+    #     response = self.client.post("/predict", json=self.ex_json_str)
+    #     self.assertEqual(response.status_code, 200)
